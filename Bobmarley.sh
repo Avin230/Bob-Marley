446558fc6bfca26d4829348876a45f137aaddf80
HGPMAC112 <HGPMAC112@MacBook-Air-6.local>
2018-10-06 14:36:32 -0700

    successful

---
+++ Bobmarley.sh
@@ -0,0 +1,27 @@
+cd ~/Desktop
+mkdir Bobmarley
+cd Bobmarley
+mkdir Cedellamarley Ziggymarley Ky-manimarley
+cd Cedellamarley 
+mkdir Skipmarley
+cd Skipmarley
+mkdir sm_songs
+cd sm_songs
+touch lions.reggae lifeboat.reggae
+cd ../../../
+cd Ziggymarley
+touch daughter_1.child daughter_2.child
+mkdir zm_songs
+cd zm_songs
+touch drive.reggae a_lifetime.reggae 
+cd ../../
+cd Ky-manimarley
+mkdir km_songs 
+mkdir movies
+cd km_songs
+touch dear_dad.reggae mama.reggae
+cd ../
+cd movies
+touch shottaz.movie one_love.movie
+
+
